e5aec27058d14e4daa36ee8f2d928a3f829ce827
JKrafty <kraft.jackson@gmail.com>
2024-04-22 12:25:33 -0500

    Lab 7 instructions upload

---
+++ Lab/Lab 8/CS400Lab8StringFrequency_name_WSUID.cpp
@@ -0,0 +1,67 @@
+// CS400Lab8StringFrequency.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <unordered_map>
+// TODO: include the header file for std::stringstream
+
+
+
+// Prints frequencies of 
+// individual words in str
+void printFrequencies(const std::string& str)
+{   
+
+    // TODO:
+    // declaring map of <string, int> type, 
+    // each word is mapped to its frequency
+    
+
+
+
+
+    // TODO:
+    // breaking input into word using 
+    // string stream
+    // Used for breaking words
+    
+
+
+    // TODO:
+    // To store individual words
+    
+
+
+
+
+
+
+    // TODO:
+    // now iterating over word, freq pair 
+    // and printing them in <, > format
+    
+
+
+
+
+
+
+
+
+}
+
+// Test code
+int main()
+{
+    std::string str = "do not let trouble trouble you do";
+    printFrequencies(str);
+    return 0;
+}
+
+
+// your output: 
+// (you, 1)
+// (do, 2)
+// (let, 1)
+// (not, 1)
+// (trouble, 2)
